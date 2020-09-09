e3755505f905b9c5d444674e213e4278b5909e17
Sudarshan Agrawal <sudarshan.shubham@gmail.com>
2020-09-09 17:35:40 -0400

    Made new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
-      <h1>Interior Designer</h1>
+      <h1>Yasha Design Studio</h1>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/work" component={Work} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
     </div>
   );
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,58 @@ import React, { Component } from "react";
 
 export class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <div>
+        <div>
+          <h2>Yasha Agrawal</h2>
+        </div>
+        <div>
+          <p>
+            Deserunt deserunt non consectetur amet nulla tempor nostrud aute
+            irure officia. Amet officia amet ipsum esse tempor laboris ad amet
+            occaecat excepteur. Et laborum magna sunt adipisicing sit
+            exercitation aliqua occaecat consectetur pariatur duis consequat
+            occaecat. Commodo do ut aliquip excepteur ea ipsum in enim
+            consectetur excepteur Lorem non.
+            <br />
+            Adipisicing cupidatat aliqua sunt ipsum esse dolore velit dolor
+            tempor ullamco excepteur irure sit ullamco. Ut nisi exercitation
+            nisi non consectetur. Cupidatat minim est dolore aliqua voluptate.
+            Aute eiusmod id anim est duis est ipsum. Ad ea occaecat dolor ea
+            enim aliqua eu proident fugiat veniam.
+          </p>
+        </div>
+
+        <div>
+          <h3>Services</h3>
+          <ul>
+            <li>PROJECT MANAGEMENT</li>
+            <p>
+              Cupidatat non nostrud eu aute nisi fugiat culpa incididunt
+              ut.Eiusmod nostrud incididunt Lorem consequat veniam duis ea sit
+              ipsum.
+            </p>
+            <li>INTERIOR DESIGN</li>
+            <p>
+              Anim consequat sint fugiat dolor ex irure proident laboris
+              occaecat.In velit cupidatat nostrud eu duis exercitation culpa
+              commodo enim irure mollit eiusmod voluptate.
+            </p>
+            <li>DESIGN CONSULTATION</li>
+            <p>
+              Ad pariatur amet esse enim nisi cillum dolor aute anim nulla.Enim
+              reprehenderit consequat quis dolore anim velit sint nisi
+              incididunt irure adipisicing.
+            </p>
+            <li>FLOOR PLANS</li>
+            <p>
+              Irure sint officia consectetur laborum dolor est ut pariatur enim
+              nostrud.Eiusmod velit reprehenderit id do nulla reprehenderit.
+            </p>
+          </ul>
+        </div>
+      </div>
+    );
   }
 }
 
